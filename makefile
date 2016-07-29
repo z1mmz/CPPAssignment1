d2663ebefdfd5e9fada4a20a1fae6cd6bd093ae7
Wolf Zimmermann <wolfz@live.com>
2016-07-29 17:58:13 +1000

    added help

---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -std=c++14 -Wall -pedantic src/*.cpp src/*.h -o mazer
--- src/mazer.cpp
+++ src/mazer.cpp
@@ -4,8 +4,17 @@
 #include <string.h>
 #include <math.h>
 #include "mazegen.h"
-
+using namespace std;
 int main(int argc, char *argv[]) {
-
+std::string help = "./mazer --lb filename.maze --sv filename.svg # load binary file and save svg file \n ./mazer --g seed --sb filename.maze # generate with seed value, save binary file \n./mazer --g seed --sv filename.svg # generate with seed value, save svg file \n ./mazer --g seed --sb filename.maze --sv filename.svg # gen with seed, save binary, save svg";
+if (argc  <= 1 || argc > 6){
+  std::cerr << "ERROR: incorrect usage please use mazer --help for commands" << std::endl;
+  std::cerr << help << std::endl;
+}else{
+  std::cout << argv[1] << std::endl;
+  if(strcmp(argv[1],"--help") == 0){
+    std::cout << help << std::endl;
+  }
+}
   return 0;
 }
