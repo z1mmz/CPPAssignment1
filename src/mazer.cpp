63bd45a31c81c9f4b52c1b9ceac5731bab61fa4f
Wolf Zimmermann <wolfz@live.com>
2016-08-25 10:34:08 +1000

    main menu

--- src/mazer.cpp
+++ src/mazer.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     std::string help = "./mazer --lb filename.maze --sv filename.svg"
-            "# load binary file and save svg file \n "
+            "# load binary file and save svg file \n"
             "./mazer --g seed --sb filename.maze # generate with seed value,"
             " save binary file \n./mazer --g seed --sv filename.svg # generate"
-            " with seed value, save svg file \n ./mazer --g seed --sb filename.maze"
+            " with seed value, save svg file \n./mazer --g seed --sb filename.maze"
             " --sv filename.svg # gen with seed, save binary, save svg";
     if (argc <= 1 || argc > 6) {
         std::cerr << "ERROR: incorrect usage p1ease use mazer --help for commands" << std::endl;
@@ -27,25 +27,31 @@ int main(int argc, char *argv[]) {
             std::cout << "maze h: " << loadedMaze.height << " maze w: " << loadedMaze.width << std::endl;
             std::cout << "total edges: " << loadedMaze.edges_v.size() << std::endl;
             std::cout << "Edge 1: " << loadedMaze.edges_v[0].x1 << std::endl;
-            loadedMaze.saveToSVG("a.svg");
+//            loadedMaze.saveToSVG("a.svg");
 
         }
-        if(strcmp(argv[1],"-g") ==0){
+        if(strcmp(argv[1],"-g") == 0){
+          if (argc < 5){
+            printf("%s\n","generator args not supplied" );
+            return 1;
+          }
             argsexeced++;
             int seed, h,w;
-            seed = (int) argv[2];
-            if(isdigit((int) argv[3]) && isdigit((int) argv[4]) ){
-                w = (int) argv[3];
+            seed = 0;
+//            if(isdigit(atoi(argv[3])) && isdigit(atoi(argv[4])) ){
+              seed = atoi(argv[2]);
+                w = atoi(argv[3]);
                 argsexeced++;
-                h = (int) argv[4];
+                h = atoi(argv[4]);
                 argsexeced++;
                 maze a = genABMaze(seed,w,h);
-            }else{
-                maze a = genABMaze(seed,10,10);
-            }
+//            }else{
+//                maze a = genABMaze(seed,10,10);
+//            }
             std::cout << "Ate: " << argsexeced << std::endl;
 
         }
+
     }
     return 0;
 }
